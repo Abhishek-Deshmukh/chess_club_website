6cc98336cd12640575fa5283b125a363a08889ed
Abhishek Anil Deshmukh <abhishek.deshmukh@niser.ac.in>
2019-06-24 14:44:48 +0530

    footer debugged

--- niser_chessclub/members/admin.py
+++ niser_chessclub/members/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from .models import Member
 
-# Register your models here.
-admin.site.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('name', 'school', 'designation', 'email')
+    list_per_page = 20
+    search_fields = ('name', 'email')
+    list_filter = ('school',)
+
+# Register or Unregister your models here.
+admin.site.register(Member, MemberAdmin)
 admin.site.unregister(Group)
