3835f8e696b2142d7db391a1cb9d8d5be681840c
Abhishek Anil Deshmukh <abhishek.deshmukh@niser.ac.in>
2019-06-24 18:29:39 +0530

    debugged the image not rendering problem, made the members database, ready to be put out after one testing

--- niser_chessclub/blog/admin.py
+++ niser_chessclub/blog/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_time')
+    list_per_page = 20
+    search_fields = ('title', 'content')
+
+admin.site.register(Post, PostAdmin)
 
 # Register your models here.
--- niser_chessclub/blog/models.py
+++ niser_chessclub/blog/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=10000, default=" ")
     image = models.ImageField(upload_to='images/', blank=True)
-    date = models.DateField(auto_now_add=True)
+    date_time = models.DateTimeField(auto_now_add=True)
 
 def __str__(self):
-    return self.date
+    return self.date_time
